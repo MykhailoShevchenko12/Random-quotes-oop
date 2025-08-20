3c02805136de2d4d0ca923c25f98d89617fb225c
MykhailoShevchenko12 <mikhailshevchenko075@gmail.com>
2025-08-20 17:08:58 +0300

    Diebled the corsOptions for correct publish working

--- server/index.js
+++ server/index.js
@@ -4,11 +4,11 @@ const quotes = require('./data/quotes');
 
 const app = express();
 const PORT = 3000;
-const corsOptions = {
-  origin: ['http://localhost:8080', 'http://127.0.0.1:8080'],
-};
+// const corsOptions = {
+//   origin: ['http://localhost:8080', 'http://127.0.0.1:8080'],
+// };
 
-app.use(cors(corsOptions));
+app.use(cors());
 
 function getRandomQuote() {
   const randomIndex = Math.floor(Math.random() * quotes.length);
